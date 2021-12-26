0c7e0c4ea6fe2b6d3a35e37fae58dc0726eef869
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2021-12-26 21:14:50 +0200

    deleted all node modules and just took one file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // using the percom library to get all the positions using permutations
-import percom from './node_modules/percom/src/permutation.js'
+import percom from './permutation.js'
 //change the background and center the circles container
 $('body').css({
     backgroundColor: '#212121',
@@ -51,7 +51,7 @@ function generatePositions() {
     //this expression will see which is less, then it will generate a number of positions based on it.
     const maxPosition = (width < height ? width : height) / 2
     //all the available positions that we will use permutations on
-    for (let number = maxPosition * -1 + 160; number < maxPosition; number += 160) {
+    for (let number = maxPosition * -1 + 160; number < maxPosition; number += 100) {
         positions.push(number)
     }
     // returning the values that are not in the occupied array
@@ -65,21 +65,20 @@ let availablePositions = generatePositions()
 
 function createCircle() {
     //this function will be always called whenever the player clicks the screen or clicks any circle. it creates the position of the center of the circle.
-    console.log(availablePositions)
+
     //get a random position from the available positions
     const idx = getRandomNumber(availablePositions.length)
     let position = availablePositions[idx]
     //store the value of that index inside the occupied array and remove it from the available positions
     occupied.push(position)
     availablePositions = availablePositions.filter(p => !arraysEqual(position,p))
-    console.log(availablePositions)
     //create a circle and add it to the DOM
     const circle = $('<div></div>').addClass('circle').css({
         position: 'absolute',
         left: position[0],
         top: position[1],
-        width: '100px',
-        height: '100px',
+        width: '80px',
+        height: '80px',
         backgroundColor: '#396EB0',
         borderRadius: '50%',
         transform: 'translate(-50%,-50%)',
@@ -95,8 +94,8 @@ circles.on('click', '.circle', function () {
     $(this).remove()
     //remove the position from the occupied array and return it back to be an available position
     occupied = occupied.filter(p => !arraysEqual(position,p))
-    availablePositions.push(position)
     createCircle()
+    availablePositions.push(position)
 })
 //add an event listener to the document to calculate the total number of clicks
 $(document).click(() => {
